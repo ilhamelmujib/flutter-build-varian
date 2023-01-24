03e444930b4d68bceba3de782d69efb98dbfb3a8
ilhamelmujib <ilham.elmujib@gmail.com>
2023-01-24 10:23:46 +0700

    first commit

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'flavor_config.dart';
+import 'home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: FlavorConfig.instance.values.titleApp,
+      theme: ThemeData(
+        primarySwatch: FlavorConfig.instance.color,
+      ),
+      home: const Home(),
+    );
+  }
+}
